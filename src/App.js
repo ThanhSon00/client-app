a2249f779978c55c53f086d8adfcae3c7669a80d
ThanhSon00 <phanson999999@gmail.com>
2024-01-01 10:07:22 +0700

    Debug 4

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Home from './containers/home/Home';
 // import AnswerPage from './containers/answer/AnswerPage'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import MainPage from './containers/main-page/MainPage';
+import MainPage from './containers/main-page/MainPage';
 import Page from './containers/page/Page';
 import Login from './containers/login/Login';
 import ForgotPassword from './containers/forgot-password/ForgotPassword';
 import ResetPassword from './containers/reset-password/ResetPassword';
 // import AskQuestion from './containers/ask-question/AskQuestion';
 // import NewsPage from './containers/news-page/NewsPage';
-// import ProtectedRoute from 'utils/ProtectedRoute';
+import ProtectedRoute from 'utils/ProtectedRoute';
 import useUser from 'hooks/useToken'
 import LoginRoute from 'utils/LoginRoute';
 // import DashBoard from 'containers/dash-board/DashBoard';
@@ -33,14 +33,14 @@ const App = () => {
           <Route path="/page/reset-password/:id" element={<ResetPassword />} />
         </Route>
 
-        {/* <Route path="/main-page/" element={<ProtectedRoute><MainPage /></ProtectedRoute>}>
-          <Route path="/main-page/dash-board" element={<DashBoard setUser={setUser}/>} />
+        <Route path="/main-page/" element={<ProtectedRoute><MainPage /></ProtectedRoute>}>
+          {/* <Route path="/main-page/dash-board" element={<DashBoard setUser={setUser}/>} />
           <Route path="/main-page/ask-question" element={<AskQuestion />} />
           <Route path="/main-page/news/" element={<NewsPage />}>
             <Route path="/main-page/news/question/:id" element={<AnswerPage />} />
             <Route path="/main-page/news/question" element={<QuestionPage />} />  
-          </Route>
-        </Route> */}
+          </Route> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   )
